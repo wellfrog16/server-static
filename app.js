0e1a93efe97a597f947ae10d1c6927c5b85d2fe4
wellfrog <wellfrog16@gmail.com>
2017-11-23 14:33:26 +0800

    更新路径

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var static = require('node-static');
 
 //
-// Create a node-static server instance to serve the './public' folder
+// Create a node-static server instance to serve the '/usr/local/src/www' folder
 //
-var file = new static.Server('./public');
+var file = new static.Server('/usr/local/src/www');
 
 require('http').createServer(function (request, response) {
    request.addListener('end', function () {
